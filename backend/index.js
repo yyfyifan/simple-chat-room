9d33ff672461870bfb89a4062d02feb8bbde5ff0
yyfyifan <yyfyifan@gmail.com>
2024-01-24 00:46:36 -0800

    Install prettier

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.port || 10001;
 
-app.get('/', (req, res) => {
-  res.send('Hello World222!');
+app.get("/", (req, res) => {
+  res.send("Hello World222!");
 });
 
 app.listen(port, () => {
