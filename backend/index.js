dcc47a5afe238345177756cc853fb20be6a4b4b1
yyfyifan <yyfyifan@gmail.com>
2024-01-24 01:39:00 -0800

    Add convenient npm command to start dev both apps

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
-const port = process.env.port || 10001;
+const port = process.env.port || 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World222!");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Service is listening on "http://localhost:${port}`);
 });
