6b0ba72d3c5fe745652df7d713ed1479548d0093
yyfyifan <yyfyifan@gmail.com>
2024-01-24 00:34:39 -0800

    Create the project directory for backend and frontend

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.port || 10001;
+
+app.get('/', (req, res) => {
+  res.send('Hello World222!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
